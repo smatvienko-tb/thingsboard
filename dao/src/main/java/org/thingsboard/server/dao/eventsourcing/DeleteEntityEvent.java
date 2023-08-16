077cec032df39f1a805976d3ab71f32313057ffb
Sergey Matvienko <smatvienko@thingsboard.io>
2023-08-16 10:58:45 +0200

    DeleteEntityEvent default ts added. POJO is still immutable. test added

--- dao/src/main/java/org/thingsboard/server/dao/eventsourcing/DeleteEntityEvent.java
+++ dao/src/main/java/org/thingsboard/server/dao/eventsourcing/DeleteEntityEvent.java
@@ -21,11 +21,28 @@ import org.thingsboard.server.common.data.id.EdgeId;
 import org.thingsboard.server.common.data.id.EntityId;
 import org.thingsboard.server.common.data.id.TenantId;
 
-@Builder
+@Builder(builderClassName = "Builder", toBuilder = true)
 @Data
 public class DeleteEntityEvent<T> {
+    private final long ts;
     private final TenantId tenantId;
     private final EntityId entityId;
     private final EdgeId edgeId;
     private final T entity;
+
+    // Use static class to avoid name collision with Lombok's generated Builder.
+    public static class Builder<T> {
+        private long ts = Long.MIN_VALUE;
+
+        // Custom build method.
+        public DeleteEntityEvent<T> build() {
+            // If ts has not been set, set it to the current time.
+            if (this.ts == Long.MIN_VALUE) {
+                this.ts = System.currentTimeMillis();
+            }
+
+            // Call the original build method.
+            return new DeleteEntityEvent<>(ts, tenantId, entityId, edgeId, entity);
+        }
+    }
 }
